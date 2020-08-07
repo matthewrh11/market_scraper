3a0bb8bf7883b8e32906d5da4bf28020b9f09a31
mattziggs <mrh470@mun.ca>
2020-08-06 23:44:28 -0230

    basics added

--- src/scrape.py
+++ src/scrape.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+import bs4
+import requests
+from bs4 import BeautifulSoup
+
+symbol = input("Enter the stock symbol you are interested in: \n")
+exchange = input("Enter the market this stock is traded on: \n")
+
+print(symbol, exchange)
